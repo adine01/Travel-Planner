cc18c4d318e15b8db7c57999da9359103e17b89d
Amarasena <amarasenaisuru@gmail.com>
2025-02-26 22:49:17 +0530

    minor issue

--- main.tf
+++ main.tf
@@ -130,26 +130,6 @@ resource "aws_instance" "web" {
   }
 }
 
-# Security group for RDS
-resource "aws_security_group" "allow_postgres" {
-  name   = "allow_postgres"
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 # RDS Instance with basic configuration
 resource "aws_db_instance" "default" {
   identifier           = "wanderwise-db"
