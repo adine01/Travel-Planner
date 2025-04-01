8b93ba051ef765654cf8a9da6311637037284e98
Amarasena <amarasenaisuru@gmail.com>
2025-04-01 16:36:48 +0530

    make a small change "welcome to wanderwisee"

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         transition={{ duration: 0.5 }}
         className="text-4xl font-bold mb-8 text-center font-display text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-teal-400"
       >
-        Welcome to WanderWise
+        Welcome to WanderWisee
       </motion.h1>
       <motion.div
         initial={{ opacity: 0, y: 20 }}
